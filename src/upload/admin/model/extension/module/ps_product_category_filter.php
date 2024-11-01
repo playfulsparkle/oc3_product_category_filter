234e5930c6ad3120411e2b859e88d81ead1e4506
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-01 17:29:32 +0100

    category filter cleanup performance

--- src/upload/admin/model/extension/module/ps_product_category_filter.php
+++ src/upload/admin/model/extension/module/ps_product_category_filter.php
@@ -3,28 +3,21 @@ class ModelExtensionModulePsProductCategoryFilter extends Model
 {
     public function cleanupCategoryFilters($category_id)
     {
-        $query = $this->db->query("SELECT filter_id FROM " . DB_PREFIX . "category_filter WHERE category_id = '" . (int) $category_id . "'");
+        $category_filter_query = $this->db->query("SELECT `filter_id` FROM `" . DB_PREFIX . "category_filter` WHERE `category_id` = '" . (int) $category_id . "'");
 
-        if ($query->rows) {
-            $removeFilters = array();
+        $category_filter_ids = array_column($category_filter_query->rows, 'filter_id');
 
-            foreach ($query->rows as $result) {
-                $sql = "SELECT COUNT(DISTINCT p.product_id) AS total" .
-                    " FROM " . DB_PREFIX . "product_to_category p2c" .
-                    " LEFT JOIN " . DB_PREFIX . "product_filter pf ON (p2c.product_id = pf.product_id AND pf.filter_id = '" . $result['filter_id'] . "') " .
-                    " LEFT JOIN " . DB_PREFIX . "product p ON (p.product_id = pf.product_id)" .
-                    " LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "') " .
-                    " WHERE p2c.category_id = '" . (int) $category_id . "'";
+        if ($category_filter_ids) {
+            $product_filter_query = $this->db->query("SELECT DISTINCT pf.filter_id FROM `oc_product_to_category` p2c
+                INNER JOIN oc_product_filter pf ON pf.product_id = p2c.product_id AND pf.filter_id IN (" . implode(", ", $category_filter_ids) . ")
+                WHERE p2c.category_id = '" . (int) $category_id . "'");
 
-                $query = $this->db->query($sql);
+            $product_filter_ids = array_column($product_filter_query->rows, 'filter_id');
 
-                if ($query->rows && $query->row['total'] == 0) {
-                    $removeFilters[] = $result['filter_id'];
-                }
-            }
+            $delete_filter_id = array_diff($category_filter_ids, $product_filter_ids);
 
-            if ($removeFilters) {
-                $this->db->query("DELETE FROM " . DB_PREFIX . "category_filter WHERE category_id = '" . (int) $category_id . "' AND filter_id IN (" . implode(", ", $removeFilters) . ")");
+            if ($delete_filter_id) {
+                $this->db->query("DELETE FROM " . DB_PREFIX . "category_filter WHERE category_id = '" . (int) $category_id . "' AND filter_id IN (" . implode(", ", $delete_filter_id) . ")");
             }
         }
     }
