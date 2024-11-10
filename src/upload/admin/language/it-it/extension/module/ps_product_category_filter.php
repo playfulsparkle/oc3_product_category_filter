83107884ce9ffc93e5c79f3e3a8b2ea6a08f53b9
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 13:13:11 +0100

    Italian language

---
+++ src/upload/admin/language/it-it/extension/module/ps_product_category_filter.php
@@ -0,0 +1,30 @@
+<?php
+// Intestazione
+$_['heading_title']            = 'Playful Sparkle - Product to Category filter';
+$_['heading_getting_started']  = 'Inizio';
+$_['heading_setup']            = 'Impostazione del filtro prodotto per categoria';
+$_['heading_troubleshot']      = 'Risoluzione dei problemi comuni';
+$_['heading_contact']          = 'Contatta il supporto';
+
+// Testo
+$_['text_extension']           = 'Estensioni';
+$_['text_success']             = 'Successo: Hai modificato correttamente il modulo Product to Category filter!';
+$_['text_filter_success']      = 'Successo: I filtri non utilizzati sono stati rimossi con successo!';
+$_['text_edit']                = 'Modifica il modulo Product to Category filter';
+$_['text_getting_started']     = '<p><strong>Panoramica:</strong> L\'estensione Playful Sparkle - Product to Category Filter migliora il tuo negozio OpenCart 3.x semplificando la gestione dei filtri all\'interno delle categorie. Al momento del salvataggio delle informazioni del prodotto, questa estensione collega automaticamente i filtri associati ai prodotti alle categorie pertinenti, risparmiando tempo e garantendo coerenza nella configurazione dei filtri.</p><p><strong>Requisiti:</strong> OpenCart 3.x+, PHP 7.3 deve essere installato e correttamente configurato.</p>';
+$_['text_setup']               = '<p>Per configurare l\'estensione, installala e attivala tramite il pannello di amministrazione di OpenCart. Vai su Estensioni > Moduli, individua Playful Sparkle - Product to Category Filter e clicca sul pulsante Abilita.</p>';
+$_['text_troubleshot']         = '<ul><li><strong>Non riesco a vedere il pulsante Rimuovi filtri non utilizzati:</strong> Assicurati che l\'estensione sia attivata e che la modifica sia abilitata e aggiornata.</li><li><strong>I filtri non sono aggiunti alla categoria:</strong> Assicurati che l\'estensione sia attivata e che la modifica sia abilitata e aggiornata.</li><li><strong>Non vedo i filtri sulla pagina principale:</strong> Devi abilitare il modulo filtri e assegnarlo a un layout.</li></ul>';
+$_['text_contact']             = '<p>Per ulteriore assistenza, contatta il nostro team di supporto:</p><ul><li><strong>Contatto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentazione:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentazione utente</a></li></ul>';
+
+// Tab
+$_['tab_general']              = 'Generale';
+$_['tab_help_and_support']     = 'Aiuto &amp; Supporto';
+
+// Voce
+$_['entry_status']             = 'Stato';
+
+// Pulsante
+$_['button_remove_filter']     = 'Rimuovi filtri non utilizzati';
+
+// Errore
+$_['error_permission']         = 'Attenzione: Non hai il permesso di modificare il modulo Product to Category filter!';
