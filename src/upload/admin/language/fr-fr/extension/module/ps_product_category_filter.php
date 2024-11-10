f8a6b991c2d392504dfa402270f02eec92111618
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 13:25:42 +0100

    French language

---
+++ src/upload/admin/language/fr-fr/extension/module/ps_product_category_filter.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']            = 'Playful Sparkle - Product to Category filter';
+$_['heading_getting_started']  = 'Commencer';
+$_['heading_setup']            = 'Configuration du Product to Category filter';
+$_['heading_troubleshot']      = 'Dépannage Courant';
+$_['heading_contact']          = 'Contacter le Support';
+
+// Text
+$_['text_extension']           = 'Extensions';
+$_['text_success']             = 'Succès : Vous avez modifié le module Product to Category filter !';
+$_['text_filter_success']      = 'Succès : Les filtres non utilisés ont été supprimés avec succès !';
+$_['text_edit']                = 'Modifier le module Product to Category filter';
+$_['text_getting_started']     = '<p><strong>Présentation :</strong> L\'extension Playful Sparkle - Product to Category filter améliore votre boutique OpenCart 3.x en simplifiant la gestion des filtres au sein des catégories. Lors de l\'enregistrement des informations sur un produit, cette extension associe automatiquement les filtres de produit aux catégories correspondantes, vous faisant ainsi gagner du temps et garantissant la cohérence de votre configuration de filtres.</p><p><strong>Exigences :</strong> OpenCart 3.x+, PHP 7.3 doit être installé et correctement configuré.</p>';
+$_['text_setup']               = '<p>Pour configurer l\'extension, installez-la et activez-la via le panneau d\'administration OpenCart. Accédez à Extensions > Modules, localisez le Playful Sparkle - Product to Category filter, puis cliquez sur le bouton Activer.</p>';
+$_['text_troubleshot']         = '<ul><li><strong>Impossible de voir le bouton Supprimer les filtres inutilisés :</strong> Assurez-vous que l\'extension est activée et que la modification a bien été activée et rafraîchie.</li><li><strong>Les filtres ne sont pas ajoutés à la catégorie :</strong> Assurez-vous que l\'extension est activée et que la modification a bien été activée et rafraîchie.</li><li><strong>Les filtres ne sont pas visibles sur la page d\'accueil :</strong> Vous devez activer le module de filtres et l\'assigner à un modèle.</li></ul>';
+$_['text_contact']             = '<p>Pour toute assistance supplémentaire, veuillez contacter notre équipe de support :</p><ul><li><strong>Contact :</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentation :</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentation de l\'Utilisateur</a></li></ul>';
+
+// Tab
+$_['tab_general']              = 'Général';
+$_['tab_help_and_support']     = 'Aide &amp; Support';
+
+// Entry
+$_['entry_status']             = 'Statut';
+
+// Button
+$_['button_remove_filter']     = 'Supprimer les filtres inutilisés';
+
+// Error
+$_['error_permission']         = 'Avertissement : Vous n\'avez pas la permission de modifier le module Product to Category filter !';
