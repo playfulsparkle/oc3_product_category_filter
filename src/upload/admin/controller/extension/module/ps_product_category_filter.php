02374c5a33dc62d9ca979eec8ee341b56d0f6a3c
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-30 18:01:04 +0200

    Update translation, add support website URL

--- src/upload/admin/controller/extension/module/ps_product_category_filter.php
+++ src/upload/admin/controller/extension/module/ps_product_category_filter.php
@@ -7,9 +7,14 @@ class ControllerExtensionModulePsProductCategoryFilter extends Controller
     const EXTENSION_EMAIL = 'support@playfulsparkle.com';
 
     /**
-     * @var string The documentation URL for the extension.
+     * @var string The URL to the support website.
      */
-    const EXTENSION_DOC = 'https://github.com/playfulsparkle/oc3_product_category_filter.git';
+    const SUPPORT_URL = 'https://support.playfulsparkle.com';
+
+    /**
+     * @var string The GitHub repository URL of the extension.
+     */
+    const GITHUB_REPO_URL = 'https://github.com/playfulsparkle/oc3_product_category_filter';
 
     private $error = array();
 
@@ -62,7 +67,7 @@ class ControllerExtensionModulePsProductCategoryFilter extends Controller
             $data['module_ps_product_category_filter_status'] = (bool) $this->config->get('module_ps_product_category_filter_status');
         }
 
-        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::EXTENSION_EMAIL, self::EXTENSION_EMAIL, self::EXTENSION_DOC);
+        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::SUPPORT_URL, self::GITHUB_REPO_URL, self::EXTENSION_EMAIL);
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
